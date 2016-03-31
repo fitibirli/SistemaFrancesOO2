84c4db8ea2e2f835bdb168bc322aa1a2c1ca7e5a
fitibirli <fedeportillo92@hotmail.com>
2016-03-31 17:13:34 -0300

    Agregado todos los escenarios de caso de uso

--- SistemaFrances/src/test/TestSetCuotas.java
+++ SistemaFrances/src/test/TestSetCuotas.java
@@ -17,22 +17,15 @@ public class TestSetCuotas {
 		
 		System.out.println("----> TraerCliente idCliente=" + cliente.getIdCliente());
 
-		System.out.println(cliente.toString());
+		System.out.println(cliente.toString() + "\n");
 		
-		System.out.println("");
-		
-		Cliente cliente2 = clienteABM.traerCliente(33333333);
-		
-		System.out.println("----> TraerCliente idCliente=" + cliente2.getIdCliente());
-
-		System.out.println(cliente2.toString());
-		
-		System.out.println("");
-
 		Prestamo p = new Prestamo(new GregorianCalendar(2016, 0, 8), 5000, 10, 6, cliente);
+		
+		System.out.println("---->Crear prestamo para el cliente: " + cliente.toString() + "\n");
 		
 		cliente.addPrestamo(p);
 		
+		System.out.println("----->setCuotas() \n");
 		System.out.println("*** Imprimir Prestamo y cuotas a pagar ****");
 		
 		
@@ -44,6 +37,105 @@ public class TestSetCuotas {
 				System.out.println(cuota.toString());
 			}
 		}
+		
+		
+
+		Cliente cliente2 = clienteABM.traerCliente(22222222);
+		
+		System.out.println("----> TraerCliente idCliente=" + cliente2.getIdCliente());
+
+		System.out.println(cliente2.toString() + "\n");
+		
+		Prestamo p2 = new Prestamo(new GregorianCalendar(2016, 10, 29), 10000, 15, 12, cliente2);
+		
+		System.out.println("---->Crear prestamo para el cliente: " + cliente2.toString() + "\n");
+		
+		cliente2.addPrestamo(p2);
+		
+		System.out.println("----->setCuotas() \n");
+		System.out.println("*** Imprimir Prestamo y cuotas a pagar ****");
+		
+		
+		System.out.println(p2.toString());
+		
+		
+		for (Prestamo prestamo : cliente2.getPrestamos()) {
+			for (Cuota cuota : prestamo.getCuotas()) {
+				System.out.println(cuota.toString());
+			}
+		}
+		
+		
+		Cliente cliente3 = clienteABM.traerCliente(33333333);
+		
+		System.out.println("----> TraerCliente idCliente=" + cliente3.getIdCliente());
+
+		System.out.println(cliente3.toString() + "\n");
+		
+		Prestamo p3 = new Prestamo(new GregorianCalendar(2016, 04, 10), 14000, 16.5, 18, cliente3);
+		
+		System.out.println("---->Crear prestamo para el cliente: " + cliente3.toString() + "\n");
+		
+		cliente3.addPrestamo(p3);
+		
+		System.out.println("----->setCuotas() \n");
+		System.out.println("*** Imprimir Prestamo y cuotas a pagar ****");
+		
+		
+		System.out.println(p3.toString());
+		
+		
+		for (Prestamo prestamo : cliente3.getPrestamos()) {
+			for (Cuota cuota : prestamo.getCuotas()) {
+				System.out.println(cuota.toString());
+			}
+		}
+		
+		
+		
+		Cliente cliente4 = clienteABM.traerCliente(44444444);
+		
+		System.out.println("----> TraerCliente idCliente=" + cliente4.getIdCliente());
+
+		System.out.println(cliente4.toString() + "\n");
+		
+		Prestamo p4 = new Prestamo(new GregorianCalendar(2016, 0, 10), 6000, 12, 8, cliente4);
+		Prestamo p5 = new Prestamo(new GregorianCalendar(2016, 0, 8), 10000, 12.5, 6, cliente4);
+		
+		System.out.println("---->Crear prestamo para el cliente: " + cliente4.toString() + "\n");
+		
+		cliente4.addPrestamo(p4);
+		
+		System.out.println("----->setCuotas() \n");
+		System.out.println("*** Imprimir Prestamo y cuotas a pagar ****");
+		
+		
+		System.out.println(p4.toString());
+		
+
+		Prestamo prestamo = cliente4.getPrestamos().get(0);
+			for (Cuota cuota : prestamo.getCuotas()) {
+				System.out.println(cuota.toString());
+			}
+		
+		System.out.println("---->Crear prestamo para el cliente: " + cliente4.toString() + "\n");
+		
+		cliente4.addPrestamo(p5);
+		
+		System.out.println("----->setCuotas() \n");
+		System.out.println("*** Imprimir Prestamo y cuotas a pagar ****");
+		
+		
+		System.out.println(p5.toString());
+		
+
+		Prestamo prestamo1 = cliente4.getPrestamos().get(1);
+			for (Cuota cuota : prestamo1.getCuotas()) {
+				System.out.println(cuota.toString());
+			}
+		
+		
+		
 		
 	
 	
